49899fb7900e945aa8b2563766c95ce768d542dd
Unsolved <110182433+unsolved99@users.noreply.github.com>
2022-07-29 12:34:28 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,3 +2,31 @@
      Bravo client v1.0
      Created by Unsolved99
 */
+
+// Player
+const player = {
+    mouseCoords: {x: 0, y: 0},
+    game_sttings: {},
+    theme_ettings: {},
+}
+
+//Communication servers
+class Ogario {};
+class Agartool{};
+
+const chatApi = {
+    server1: Ogario,
+    server2: Agartool // Not sure yet what the purpose of this will be
+};
+
+// Multibox
+class Multibox {
+    constructor(){
+        this.tabs = [];
+    }
+
+};
+
+// Rendering
+
+
