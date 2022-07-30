4a6aa9f96b8ce6536245f9150f0f4c6c1d239fa4
unsolved99 <110182433+unsolved99@users.noreply.github.com>
2022-07-30 21:18:02 +0200

    Updated

---
+++ toastr.settings.js
@@ -0,0 +1,17 @@
+toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": true,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "3000",
+    "hideDuration": "3000",
+    "timeOut": "3000",
+    "extendedTimeOut": "3000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+  }
